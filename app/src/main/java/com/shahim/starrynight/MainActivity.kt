2245b1f4126347d16f82e9b0951087dce49ce269
Mohammed Shahim A <shahim.calicut@gmail.com>
2020-02-10 02:49:16 +0530

    add data provider

--- app/src/main/java/com/shahim/starrynight/MainActivity.kt
+++ app/src/main/java/com/shahim/starrynight/MainActivity.kt
@@ -2,11 +2,35 @@ package com.shahim.starrynight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.shahim.starrynight.data.DataProvider
+import com.shahim.starrynight.model.ImageObject
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private lateinit var imageList: List<ImageObject>
+
+    private fun init() {
+        val observer = DataProvider.getImageObservable(this)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = {
+                    imageList = it
+                    Toast.makeText(this,"Loaded : "+imageList.size,Toast.LENGTH_LONG).show()
+                },
+                onError = {
+                    Toast.makeText(this,"Error : "+it.message,Toast.LENGTH_LONG).show()
+                }
+            )
     }
 }
---
+++ app/src/main/java/com/shahim/starrynight/data/DataProvider.kt
@@ -0,0 +1,54 @@
+package com.shahim.starrynight.data
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.shahim.starrynight.R
+import com.shahim.starrynight.model.ImageObject
+import io.reactivex.Single
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+
+
+object DataProvider {
+
+    private var cachedImagesObservable: Single<List<ImageObject>>? = null
+    fun getImageObservable(c: Context): Single<List<ImageObject>> {
+        if(cachedImagesObservable == null) {
+            val imagesObservable: Single<List<ImageObject>> =
+                Single.create { emitter ->
+                    fun readTextFile(inputStream: InputStream): String {
+                        val outputStream = ByteArrayOutputStream()
+                        val buf = ByteArray(1024)
+                        var len: Int
+                        try {
+                            while (inputStream.read(buf).also { len = it } != -1) {
+                                outputStream.write(buf, 0, len)
+                            }
+                            outputStream.close()
+                            inputStream.close()
+                        } catch (e: IOException) {
+                            emitter.onError(e)
+                        }
+                        return outputStream.toString()
+                    }
+                    try {
+                        val xmlFileInputStream: InputStream = c.getResources().openRawResource(R.raw.data)
+
+                        val jsonString: String = readTextFile(xmlFileInputStream)
+
+                        val gson = Gson()
+                        val type = object : TypeToken<List<ImageObject>>() {}.type
+                        val images: List<ImageObject> = gson.fromJson(jsonString,type)
+                        emitter.onSuccess(images)
+
+                    } catch (e: Exception) {
+                        emitter.onError(e)
+                    }
+                }
+            cachedImagesObservable = imagesObservable.cache()
+        }
+        return cachedImagesObservable!!
+    }
+}
